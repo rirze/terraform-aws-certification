0fe0ed932d685cac35a0c1f88fc352836a28a90a
Sourabh Cheedella <cheedella.sourabh@gmail.com>
2019-02-07 17:12:11 -0500

    added patch to prevent direct IP access to instance in chp4.naive-alb

--- chapter4/naivealb/security.tf
+++ chapter4/naivealb/security.tf
@@ -10,7 +10,6 @@ resource "aws_security_group" "instance-sg" {
     from_port = 80
     to_port = 80
     protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
     security_groups = ["${aws_security_group.lb-sg.id}"]
   }
 
